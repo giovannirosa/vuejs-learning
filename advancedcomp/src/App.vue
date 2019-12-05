1abb2e0d2e842998137964c1c644b1940a454b67
Giovanni Rosa <g.rosa@cinq.com.br>
2019-12-05 11:01:45 -0300

    aaaa

--- advancedcomp/src/App.vue
+++ advancedcomp/src/App.vue
@@ -1,19 +1,24 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-               
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2 slot="title">The Quote</h2>
+          <p slot="content">A wonderful Quote!</p>
+        </app-quote>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import QuoteVue from './components/Quote.vue';
 
-    export default {
-        
-    }
+export default {
+  components: {
+    appQuote: QuoteVue
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
